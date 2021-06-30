639e33875a443bee6165e3a88a6e5df16eed23fd
Undisclosed <undisclosed674@gmail.com>
2021-06-30 03:58:25 -0700

    remove devtool

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
-  devtool: 'inline-source-map',
+  /*devtool: 'inline-source-map',*/
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
